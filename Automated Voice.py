9088aac01e90ebfdb23b500c457d65a1adc2b5dc
Sai Sasikanth Rokkam <56929863+sasikanthrok@users.noreply.github.com>
2021-07-04 12:55:28 +0530

    Create Automated Voice.py

---
+++ Automated Voice.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+from gtts import gTTS
+import os
+
+voice= ""
+
+while True:
+    e=sr.Recognizer()
+    with sr.Microphone() as source:
+        try:
+            audio=e.listen(source)
+            text=e.recognize_google(audio)
+            print(text)
+            if text=="stop":
+                break
+            text=e.recognize_google(audio)
+            voice=voice+str(text)
+            
+            
+        except:
+            print("Say something")
+            break
+            
+we=gTTS(text=voice,lang='en',slow=False)
+we.save("audio.wav")
